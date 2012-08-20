c56c60f61c0586daaf52cdaee4566889eb176286
xles McFuzzypants <Administratr@Obelix.(none)>
2012-08-20 13:08:02 +0200

    Initial commit.

---
+++ reader.js
@@ -0,0 +1,110 @@
+/**
+ * Le comment.
+ */
+function readFile(md) 
+{
+	md = typeof md !== 'undefined' ? md : true;
+
+	file = location.search.substring(1);
+	if (!file) {
+		document.body.innerHTML = 'No file specified.';
+		copyBlurb();
+		return false;
+	}
+	filename = file + '.txt';
+
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", filename, true);
+	xhr.onreadystatechange = function() {
+		document.body.innerHTML = '';
+		if (xhr.readyState == 4) {
+			if (xhr.status == 404) {
+				err = '"' + filename + '" no such file.';
+				document.body.innerHTML = err;
+			} else {
+				if(md == true)
+					text = markdown(xhr.responseText);
+				else 
+					text =    nltop(xhr.responseText);
+				
+				formatText(file, text);
+			}
+		}
+		copyBlurb();
+	}
+	xhr.send(null);
+}
+
+/**
+ * Le comment.
+ */
+function formatText(file, text)
+{
+	rawLink = document.createElement('a');
+	rawLink.href = file + '.txt';
+	rawLink.innerHTML = "View raw file.";
+	document.body.appendChild(rawLink);
+
+	document.body.appendChild(text);
+	
+	article = document.body.getElementsByTagName('div')[0];
+
+	leFirstChild = article.firstChild;
+	while (leFirstChild.nodeType != 1) 
+		leFirstChild = leFirstChild.nextSibling;
+	
+	if (leFirstChild.nodeName != 'H1') {
+		header = document.createElement('h1');
+		header.innerHTML = file.replace('-', ' ');
+		document.title = header.innerHTML;
+		article.insertBefore(header, leFirstChild);
+	} else {
+		document.title = leFirstChild.innerHTML;
+	}
+}
+
+/**
+ * Le comment.
+ */
+function markdown(str)
+{
+	var converter = new Showdown.converter();
+
+	article = document.createElement('div');
+	article.innerHTML = converter.makeHtml(str);
+	return article;
+}
+
+/**
+ * Le comment.
+ */
+function nltop(str) 
+{
+	str = str.replace(/\r\n/g,"\n");
+	str = str.replace(/\n\r/g,"\n");
+	str = str.replace(/\r/g,  "\n");
+
+	str = str.split("\n\n");
+
+	article = document.createElement('div');
+	for (line in str) {
+		if (str[line]){
+			paragraph = document.createElement('p');
+			paragraph.innerHTML = str[line];
+			article.appendChild(paragraph);
+		}
+	}
+	return article;
+}
+
+/**
+ * Le comment.
+ */
+function copyBlurb()
+{
+	copy = document.createElement('div');
+	copy.className = 'copyright';
+	copy.innerHTML = 'Text prettifier &copy; xles 2012<br>'
+			+ 'Showdown markdown parser by John Fraser';
+	document.body.appendChild(copy);
+}
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Text prettifier
+
+Ok, so I wrote this little JavaScript thing, as an exercise to learn me some
+JavaScript. But it also turned out to be a useful little script on my 
+dropbox.
+
+### So what is it?
+
+It's a text-file wrapper, so that you can view (and share) plain-text files 
+on your dropbox with a teeny tiny bit of civilized styling.
+
+Also, thanks to Showdown by John Fraser you can also get some markdown
+goodness.
+
+## Installation
+
+Just dump all the files in your dropbox. Don't want to have a bunch of files
+cluttering up your dropbox? Get the single file version from my gist here:
+https://gist.github.com/3398421
+
+(The single file version may become wonky in firefox for some reason.)
+
+## Usage
+
+First, you need a text file. For your convenience I have included, as a 
+demo, a copy of the _Constitution of the United States of America_ in
+`usa.txt`. To access this file, just go to `path/to/script/reader.html?usa`,
+piece of cake!
+
+Should work in any modern-ish browser, including IE7 (not saying that IE7
+is in any way, shape or form a modern browser, though).
+
+### Known potential issues
+
+I haven't spent much time checking how well it works with unclean file 
+names. I do know it will work with any civilised `a-z0-9._-` file name
+however.
+
+## Credits
+
+* Author of the Markdown parser "Showdown", John Fraser. Showdown can be 
+found here: https://github.com/coreyti/showdown
+
+* Author of Markdown, John Gruber.
